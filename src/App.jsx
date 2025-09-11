da9367f52bb78883b7e1a6a419ee02e706c0653d
Asha Saini <ashasainiasha0866@gmail.com>
2025-09-11 22:36:23 +0530

    addded ui of sidebar and player

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import Sidebar from './components/Sidebar'
+import Player from './components/Player'
 const App = () => {
   return (
-    <div>
-      <p className='text-green'>Hii my name is Ashish Kumar</p>
+    <div className='h-screen bg-black'>
+      {/* <p className='text-green-600'>Hii my name is Ashish Kumar</p> */}
+      <div className='h-[90%] flex '>
+        <Sidebar/>
+      </div>
+      <Player/>
     </div>
   )
 }
