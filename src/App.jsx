c2221b408596468126d5a6c6386c5ec8eb2ac924
Asha Saini <ashasainiasha0866@gmail.com>
2025-09-12 03:54:56 +0530

    Added Navbar ,displayHome,Display,albumItem in component player and Sidebar songItem and albumItem for data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Sidebar from './components/Sidebar'
 import Player from './components/Player'
+import Display from './components/Display'
 const App = () => {
   return (
     <div className='h-screen bg-black'>
       {/* <p className='text-green-600'>Hii my name is Ashish Kumar</p> */}
       <div className='h-[90%] flex '>
         <Sidebar/>
+        <Display/>
       </div>
       <Player/>
     </div>
