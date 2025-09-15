6ed4fcd9a5d8f512f99c2e8f127cf4d217f115e3
Asha Saini <ashasainiasha0866@gmail.com>
2025-09-16 00:06:11 +0530

    addedd funtionalty of time and bar of song

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useRef, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import { songsData } from "../assets/assets";
 
   export const PlayerContext = createContext()
@@ -6,7 +6,7 @@ import { songsData } from "../assets/assets";
     const audioRef  =useRef()
     const seekBg = useRef()
     const seekBar = useRef()
-    const [track,setTrack] = useState(songsData[0])//default track will be 1st song
+    const [track,setTrack] = useState(songsData[1])//default track will be 1st song
     const [playStatus,setPlayStatus]=useState(false);//pause
     const[time,setTime] = useState({
         currentTime:{
@@ -26,6 +26,23 @@ import { songsData } from "../assets/assets";
         audioRef.current.pause()
         setPlayStatus(false)
     }
+    useEffect(()=>
+    {
+        audioRef.current.ontimeupdate=()=>{
+            //for greenn bar
+            seekBar.current.style.width = (Math.floor(audioRef.current.currentTime /audioRef.current.duration * 100))+"%"
+            setTime({
+                currentTime:{
+                    second:Math.floor(audioRef.current.currentTime % 60 ),
+                    minute:Math.floor(audioRef.current.currentTime /60)
+                },
+                totalTime:{
+                    second:Math.floor(audioRef.current.duration % 60 ),
+                    minute:Math.floor(audioRef.current.duration /60)
+                }
+            })
+        }
+    })
     const contextValue={
      audioRef,seekBar,seekBg,track,setTrack,playStatus,setPlayStatus,time,setTime,play,pause
     }
